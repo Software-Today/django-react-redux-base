5d56cebb93a4b73acf48d1b31a3cade3ddef9b18
Nicoli430 <nicoli.dev.430@gmail.com>
2017-10-04 19:26:58 -0400

    Update common.config.js

--- src/static/styles/font-awesome.config.prod.js
+++ src/static/styles/font-awesome.config.prod.js
@@ -1,6 +1,6 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const fontAwesomeConfig = require('./font-awesome.config');
-const buildExtractStylesLoader = require('./font-awesome-helper');
+const buildExtractStylesLoader = require('./font-awesone-helper');
 
 fontAwesomeConfig.styleLoader = buildExtractStylesLoader(ExtractTextPlugin.extract(['css-loader', 'less-loader']));
 
